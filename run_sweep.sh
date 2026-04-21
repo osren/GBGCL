69260d80445e82d9e770d380df153c7ebd52b8bc
TanCheng <1529924810@qq.com>
2026-04-21 22:14:43 +0800

    更新run_sweep.sh

--- run_sweep.sh
+++ run_sweep.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
+
 LOGFILE="sweepX_$(date +%Y%m%d_%H%M%S).log"
 ERRFILE="sweepX_errors_$(date +%Y%m%d_%H%M%S).log"
 
 echo "Log: $LOGFILE"
 echo "Errors: $ERRFILE"
+echo "Workers: $SWEEP_WORKERS"
+echo "Stage: $SWEEP_STAGE"
 
-python tools/sweepX.py 2>&1 | tee "$LOGFILE" | grep -E "\[ERR " > "$ERRFILE"
+nohup python tools/sweepX.py > "$LOGFILE" 2>&1 &
+echo "PID: $!"
